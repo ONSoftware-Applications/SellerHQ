95efb54ee625867a4ab6691dcfe279643ec9f50f
ONSoftware <AdminONSoftware@gmail.com>
2026-09-06 14:24:11 +0100

    Load final UI v2 polish layer

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import './styles/preferences-v2.css'
 import './styles/tax-v2.css'
 import './styles/billing-v2.css'
 import './styles/account-v2.css'
+import './styles/polish-v2.css'
 
 import { AuthProvider } from './context/AuthContext'
 import { BusinessProvider } from './context/BusinessContext'
